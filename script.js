f20745533f0a0cc2b4f394b6505a98f6fee223fa
blackcrabb <niyati20002000@gmail.com>
2020-06-07 18:48:18 +0530

    styling in process

---
+++ script.js
@@ -0,0 +1,10 @@
+function enterSite() {
+    console.log("Enter Site");
+    $("#landing").css("transform","translate(-200vw)");
+    $("#dimmed-bg").css("visibility","visible");
+    $("#dimmed-bg").css("transform","translate(0)");
+    $("#above-fold").css("opacity","1");
+    $("#full-site").css("visibility","visible");
+    $("#full-site").css("overflow","auto");
+    $("#full-site").css("max-height","auto");
+}
