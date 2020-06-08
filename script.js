84c829bc880bbd16b70120c55609e25f477aae5c
blackcrabb <niyati20002000@gmail.com>
2020-06-08 22:54:04 +0530

    location and users done

--- script.js
+++ script.js
@@ -37,3 +37,30 @@ function closeForm() {
     $("#call-num").css("opacity","1");
     $("#reg").css("opacity","1");
 }
+
+(function manageTotalUsers(){
+    
+    let totalUsers = String(getRandom(100000 , 200000));
+    totalUsers = addComma(totalUsers);
+
+    addToTotalUsers(totalUsers);
+
+    function addToTotalUsers(currentValue){
+        let num = Number(currentValue.replace(",",""));
+        num++;
+        num = String(num);
+        num = addComma(num);
+        $("#total-users").html(num);
+        setTimeout(function(){
+            addToTotalUsers(num);
+        },getRandom(100,2000));
+    }
+
+    function getRandom(min , max){
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+
+    function addComma(num){
+        return num.slice(0,3) + ","+ num.slice(3,6);
+    }
+})();
